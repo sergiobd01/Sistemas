958becf2c7d2b400e5e65183784554a479f03169
Sergio01 <sergio_bautista@javeriana.edu.co>
2019-03-19 21:37:41 -0500

    Primer commit

---
+++ Sistema/Data/DbInitializer.cs
@@ -0,0 +1,32 @@
+﻿using Sistema.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize (SistemaContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Categoria.Any())
+            {
+                return;
+            }
+            var categorias = new Categoria[]
+            {
+                new Categoria {Nombre = "Precalculo", Descripcion= "Calculo basico", Eestado= true},
+                new Categoria{Nombre="Programacion", Descripcion = "Curso de programacion", Eestado =  true}
+            };
+
+            foreach(Categoria c in categorias)
+            {
+                context.Categoria.Add(c);
+            }
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Sistema/Models/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema.Models
+{
+    public class Categoria
+    {
+        public int CategoriaID { get; set; }
+        [Required(ErrorMessage = "Campo Requerido")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage ="El nombre debe tener de 3 a 50 caracteres")]
+        public string Nombre { get; set; }
+        [StringLength(256, ErrorMessage ="No debe superar los 256 caracteres")]
+        [Required]
+        public string Descripcion { get; set; }
+        public Boolean Eestado { get; set; }
+    }
+}
